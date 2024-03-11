2b94d7bf2ac027b8366af6aca0f266e53b97f5cb
santy_000 <santy_000@Saku>
2024-03-11 10:54:35 -0300

    Fausto Miranda.

---
+++ src/main/java/com/example/PyBdMarzo2024MirandaMeallaFaustoSantiagoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyBdMarzo2024MirandaMeallaFaustoSantiagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyBdMarzo2024MirandaMeallaFaustoSantiagoApplication.class, args);
+	}
+
+}
